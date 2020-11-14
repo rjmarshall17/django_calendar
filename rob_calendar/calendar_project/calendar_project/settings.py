ed2c7290e56bb5915957352a04d1aa308149f0dd
Rob Marshall <rob.marshall17@gmail.com>
2020-11-13 22:47:04 -0500

    Get secret key from local file

--- rob_calendar/calendar_project/calendar_project/settings.py
+++ rob_calendar/calendar_project/calendar_project/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '***REMOVED***'
+with open('/Users/rob/django_secret_key') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
